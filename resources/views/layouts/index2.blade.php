6771fe0e84a14bdd33a10246222393d76863fcba
danielsforero <daniel.forero93@gmail.com>
2015-10-19 08:31:09 -0500

    otro

--- resources/views/layouts/index2.blade.php
+++ resources/views/layouts/index2.blade.php
@@ -10,47 +10,7 @@
 <body>
 <div id="agrupar">
   <header id="cabeza">
-    <div class="content">
-      <div class="row">
-        <div class="content" id="contenido_carousel">
-        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-          <!-- Indicators -->
-          <ol class="carousel-indicators">
-            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-          </ol>
-
-          <!-- Wrapper for slides -->
-          <div class="carousel-inner" role="listbox">
-            <div class="item active">
-              <img src="images/energy.jpg" alt="lA PEGO">
-              <div class="carousel-caption">
-                Descripcion
-              </div>
-            </div>
-            <div class="item">
-              <img src="images/escudo.JPG" alt="...">
-              <div class="carousel-caption">
-                ...
-              </div>
-            </div>
-            ...
-          </div>
-
-          <!-- Controls -->
-          <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
-      </div>
-      </div>
-    </div>
+    
   </header>
   <nav id="menu" class="navbar navbar-inverse">
       <div class="container-fluid">
@@ -147,7 +107,53 @@
               <span class="glyphicon glyphicon-search" aria-hidden="true">ds</span>
             </div>
             <br>
+
+            <div class="content">
+      <div class="row">
+        <div class="content" id="contenido_carousel">
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+          </ol>
+
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner" role="listbox">
+            <div class="item active">
+              <img src="images/energy.jpg" alt="lA PEGO">
+              <div class="carousel-caption">
+                Descripcion
+              </div>
+            </div>
+            <div class="item">
+              <img src="images/escudo.JPG" alt="...">
+              <div class="carousel-caption">
+                ...
+              </div>
+            </div>
+            ...
           </div>
+
+          <!-- Controls -->
+          <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+      </div>
+    </div>
+
+            <!--aqui termina fila-->
+
+          </div>
+
         </div>
       </div>
   </section>
